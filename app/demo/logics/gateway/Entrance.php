33d7e47d1449aa34fc0e8b75af3b38b735bffe46
Gabriel <obdo.briel@gmail.com>
2017-06-02 15:22:38 +0800

    feat(handle/userDefinedHandle): support user defined handle when framework running :sparkles:

---
+++ app/demo/logics/gateway/Entrance.php
@@ -0,0 +1,23 @@
+<?php
+/***********************************
+ *             Gab PHP             *
+ *                                 *
+ * A light php framework for study *
+ *                                 *
+ *             Gabriel             *
+ *  <https://github.com/obdobriel> *
+ *                                 *
+ ***********************************/
+
+namespace App\Demo\Logics\Gateway;
+
+use Framework\App;
+
+class Entrance
+{
+    public function __construct()
+    {
+        echo 'Gateway';
+        die;
+    }
+}
