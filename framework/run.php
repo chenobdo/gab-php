60b199c8575d20a70db233c14ee2924be5c293d0
Gabriel <obdo.briel@gmail.com>
2017-06-06 15:15:04 +0800

    docs(App): optimize note for code :ok_hand:

--- framework/run.php
+++ framework/run.php
@@ -20,7 +20,11 @@ use Framework\Exceptions\CoreHttpException;
 use Framework\Request;
 use Framework\Response;
 
-// 引入框架文件
+/**
+ * 引入框架文件
+ *
+ * Require framework
+ */
 require(__DIR__ . '/App.php');
 
 /**
@@ -28,59 +32,81 @@ require(__DIR__ . '/App.php');
  */
 
 try {
-    /* 初始化应用 */
+    /**
+     * Init framework
+     */
     $app = new Framework\App(__DIR__ . '/..', function () {
         return require(__DIR__ . '/Load.php');
     });
 
-    /* 挂载框架handles */
+    /*
+     * Load all kinds of handles
+     */
 
     $app->load(function () {
-        // 加载错误处理机制
+        // Loading log handle
+        return new LogHandle();
+    });
+
+    $app->load(function () {
+        // Loading error handle
         return new ErrorHandle();
     });
 
     $app->load(function() {
         //  加载异常处理机制　由于本文件全局catch了异常　所以不存在未捕获异常
         //　可省略注册未捕获异常Handle
+        //　Loading exception handle.
+        //  I'm not used it, because this file catch all exception　
         return new ExceptionHandle();
     });
 
     $app->load(function() {
-        // 加载预定义配置机制
+        // Loading config handle
         return new ConfigHandle();
     });
 
     $app->load(function () {
-        // 加载日志处理机制
-        return new LogHandle();
-    });
-
-    $app->load(function () {
-        // 加载nosql机制
+        // Loading nosql handle
         return new NosqlHandle();
     });
 
     $app->load(function () {
-        // 加载用户自定义机制
+        // Loading user-defined handle
         return new UserDefinedHandle();
     });
 
     $app->load(function () {
-        // 加载路由机制
+        // Loading router handle
         return new RouterHandle();
     });
 
-    // 启动应用
+    /**
+     * 启动应用
+     *
+     * Start framework
+     */
     $app->run(function() use ($app){
         return new Request($app);
     });
 
-    // 应用生命周期结束　响应结果
+    /**
+     * 响应结果
+     *
+     * Reponse
+     *
+     * 应用生命周期结束
+     *
+     * End
+     */
     $app->response(function() {
         return new Response();
     });
 } catch (CoreHttpException $e) {
-    // 捕获异常
+    /**
+     * 捕获异常
+     *
+     * Catch exception
+     */
     $e->reponse($e);
 }
